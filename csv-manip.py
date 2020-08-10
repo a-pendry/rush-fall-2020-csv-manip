e4a3a6d831cad8d154a1b4c9103756bf9c215d1b
a-pendry <j.adam.pendry@gmail.com>
2020-08-10 11:31:49 -0500

    added error handling and fixed day indexing

--- csv-manip.py
+++ csv-manip.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import math
 
 # TODO: Replace "MOCK_DATA.csv" with the actual filename once everyone sends me their email
 
@@ -14,14 +15,16 @@ name_to_email = origin_df.set_index('name').T.to_dict('list')
 
 day = 0
 block = 0
+count = 0
 
 for filename in os.listdir(path):
     if filename.endswith(".csv") and filename != "MOCK_DATA.csv":
         dest_df = pd.read_csv(os.path.join(path, filename))
         names_to_replace = dest_df['Email Address']
 
-        day += 1
         block += 1
+        count += 1
+        day =  math.ceil(count/3) #yields the sequence 1,1,1,2,2,2,...
         block = block % 3
         new_col = []
         new_path_name = "Day{}Block{}.csv".format(day, block)
@@ -29,7 +32,15 @@ for filename in os.listdir(path):
         del dest_df['Email Address']
 
         for entry in names_to_replace:
-            new_col.append(name_to_email[entry][0])
+            try:
+                if entry != "":
+                    new_col.append(name_to_email[entry][0])
+                else:
+                    new_col.append("")
+            except KeyError as ke:
+                new_col.append("ERROR")
+                print("The name \"" + entry + "\" is missing from the email registry")
+                continue
         
         dest_df['Email Address'] = new_col
         dest_df.to_csv(os.path.join(path, new_path_name), index=False, header=True)
